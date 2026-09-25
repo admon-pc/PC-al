279610cc5ff305b4325acb76d5cb6025edd06be5
admon-pc <artemybasov@gmail.com>
2026-09-25 21:06:06 +0300

    update

--- src/al/Audio/alAudioEngineWASAPI.cpp
+++ src/al/Audio/alAudioEngineWASAPI.cpp
@@ -197,6 +197,8 @@ bool alAudioEngineWASAPI::Initialize()
 		return false;
 	}
 
+	printf("m_bufferSize: %u\n", m_bufferSize);
+
 	hr = m_audioClient->GetService(IID_PPV_ARGS(&m_renderClient));
 	if (FAILED(hr))
 	{
@@ -219,6 +221,8 @@ bool alAudioEngineWASAPI::Initialize()
 			return false;
 		}
 	}
+	
+	m_audioClient->Start();
 
 	AL_SAFERELEASE(m_deviceEnumerator);
 	return true;
@@ -229,11 +233,177 @@ void alAudioThreadFunction_WASAPI(alAudioEngineWASAPI* engine)
 	engine->m_run = true;
 	auto di = engine->GetDeviceInfo();
 
-	uint8_t* wave = (uint8_t*)malloc(2);
-	
+	HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	if (FAILED(hr))
+	{
+		printf("Unable to initialize COM in render thread: %x\n", hr);
+		return;
+	}
+
+	Sleep(100);
+
+	uint8_t* wave = (uint8_t*)malloc(di.m_bytesPerSecond);
+	if (wave)
+	{
+		auto wave_ptr = wave;
+
+		uint16_t* pcm16_ch1 = (uint16_t*)wave_ptr;
+		uint16_t* pcm16_ch2 = pcm16_ch1 + 1;
+
+		float32_t* pcm32_ch1 = (float32_t*)wave_ptr;
+		float32_t* pcm32_ch2 = pcm32_ch1 + 1;
+
+
+		float64_t angle_step = (240.0 * PIPI) / (float64_t)di.m_sampleRate;
+		float64_t angle = 0.f;
+		for (uint32_t i = 0; i < di.m_sampleRate; ++i)
+		{
+			float64_t sn = sin(angle);
+			
+			/*
+			float32_t t = (float32_t)i / (float32_t)di.m_sampleRate;
+			float sn = sinf(PIPIf*240.f*t);
+			*/
+			switch (di.m_format)
+			{
+			case alAudioFormat::PCM_16:
+				break;
+			case alAudioFormat::PCM_32_float:
+				*pcm32_ch1 = sn;
+				if (di.m_channels == 2)
+				{
+					*pcm32_ch2 = sn;
+				}
+
+				wave_ptr += di.m_bytesPerBlock;
+				pcm32_ch1 = (float32_t*)wave_ptr;
+				pcm32_ch2 = pcm32_ch1 + 1;
+				break;
+			}
+
+			angle += angle_step;
+		}
+
+		/*FILE* f = 0;
+		fopen_s(&f, "wave.raw", "wb");
+		if (f)
+		{
+			fwrite(wave, 1, di.m_bytesPerSecond, f);
+			fclose(f);
+		}*/
+	}
+	//engine->m_audioClient->Start();
+
+	UINT32 data_position = 0;
 	while (engine->m_run)
 	{
 		Sleep(10);
+
+		if (wave)
+		{
+
+			BYTE* pData = 0;
+			UINT32 padding = 0;
+			UINT32 framesAvailable = 0;
+		
+			hr = engine->m_audioClient->GetCurrentPadding(&padding);
+			if (SUCCEEDED(hr))
+			{
+				framesAvailable = engine->m_bufferSize - padding;
+				//while ((_RenderBufferQueue->_BufferLength <= (framesAvailable * _FrameSize)))
+				{
+				//	printf("engine->m_bufferSize: %u\n", engine->m_bufferSize);
+				//	printf("framesAvailable: %u\n", framesAvailable);
+					hr = engine->m_renderClient->GetBuffer(framesAvailable, &pData);
+					if (SUCCEEDED(hr))
+					{
+						//
+						//  Copy data from the render buffer to the output buffer and bump our render pointer.
+						uint32_t copy_sz = framesAvailable * di.m_bytesPerBlock;
+						/*if ((data_position + copy_sz) > di.m_bytesPerSecond)
+						{
+							copy_sz = (data_position + copy_sz) - di.m_bytesPerSecond;
+							CopyMemory(pData, &wave[data_position], copy_sz);
+						}
+						else*/
+						{
+							uint32_t copy_available = di.m_bytesPerSecond - data_position;
+							if (copy_sz > copy_available)
+							{
+								//copy_sz = copy_available;
+								printf("tik\n");
+								CopyMemory(pData, &wave[data_position], copy_available);
+
+								data_position = 0;
+								// if repeat
+								CopyMemory(&pData[copy_available], &wave[data_position], copy_sz - copy_available);
+								data_position = copy_sz - copy_available;
+								data_position -= copy_sz;
+							}
+							else
+							{
+								CopyMemory(pData, &wave[data_position], copy_sz);
+							}
+						}
+
+						data_position += copy_sz;
+						if (data_position >= di.m_bytesPerSecond)
+							data_position = 0;
+
+						hr = engine->m_renderClient->ReleaseBuffer(framesAvailable, 0);
+						if (!SUCCEEDED(hr))
+						{
+							printf("Unable to release buffer: %x\n", hr);
+						}
+					}
+					else
+					{
+						printf("Unable to get buffer: %x\n", hr);
+						switch (hr)
+						{
+						case AUDCLNT_E_BUFFER_ERROR:
+							printf("AUDCLNT_E_BUFFER_ERROR\n");
+							break;
+						case AUDCLNT_E_BUFFER_TOO_LARGE:
+							printf("AUDCLNT_E_BUFFER_TOO_LARGE\n");
+							break;
+						case AUDCLNT_E_BUFFER_SIZE_ERROR:
+							printf("AUDCLNT_E_BUFFER_SIZE_ERROR\n");
+							break;
+						case AUDCLNT_E_OUT_OF_ORDER:
+							printf("AUDCLNT_E_OUT_OF_ORDER\n");
+							break;
+						case AUDCLNT_E_DEVICE_INVALIDATED:
+							printf("AUDCLNT_E_DEVICE_INVALIDATED\n");
+							break;
+						case AUDCLNT_E_RESOURCES_INVALIDATED:
+							printf("AUDCLNT_E_RESOURCES_INVALIDATED\n");
+							break;
+						case AUDCLNT_E_BUFFER_OPERATION_PENDING:
+							printf("AUDCLNT_E_BUFFER_OPERATION_PENDING\n");
+							break;
+						case AUDCLNT_E_SERVICE_NOT_RUNNING:
+							printf("AUDCLNT_E_SERVICE_NOT_RUNNING\n");
+							break;
+						case E_POINTER:
+							printf("E_POINTER\n");
+							break;
+						}
+					}
+
+					hr = engine->m_audioClient->GetCurrentPadding(&padding);
+					if (SUCCEEDED(hr))
+					{
+						framesAvailable = engine->m_bufferSize - padding;
+					}
+					else
+					{
+						printf("Unable to get current padding: %x\n", hr);
+					}
+				}
+			}
+		}
+
 
 		while (!engine->m_queue.empty())
 		{
@@ -253,4 +423,6 @@ void alAudioThreadFunction_WASAPI(alAudioEngineWASAPI* engine)
 	{
 		free(wave);
 	}
+
+	CoUninitialize();
 }
